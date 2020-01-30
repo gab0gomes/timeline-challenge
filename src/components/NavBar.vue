8888bc49b2206008cca690270afb3145f862c442
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2020-01-29 23:56:55 -0300

    Cria componente da NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="nav">
+    <img
+      class="logo"
+      alt="Logo"
+      src="../assets/img/logo.svg"
+    />
+    <router-link to="/">Timeline</router-link>
+    <router-link to="/about">Sobre</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+
+.nav {
+  padding: 15px 0px;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  background-color: #303F66;
+  box-shadow: 0 1px 2px 0 rgba(0,0,0,0.5);
+
+  .logo {
+    max-width: 40px;
+    margin: 0em 2em;
+  }
+
+  a {
+    color: #FFF;
+    text-decoration: none;
+    margin-right: 2em;
+    font-weight: bold;
+  }
+
+  a:hover, a.router-link-exact-active {
+    color: #36B274;
+  }
+
+}
+</style>

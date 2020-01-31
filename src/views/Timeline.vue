85a55ad764abf363addc80e981f61cb6dbde0940
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2020-01-30 23:09:11 -0300

    Cria componente Timeline

---
+++ src/views/Timeline.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="timeline">
+    <timeline-list
+    />
+  </div>
+</template>
+
+<script>
+import TimelineList from '../components/TimelineList.vue';
+
+export default {
+  name: 'home',
+  components: {
+    TimelineList,
+  },
+};
+</script>
+
+<style lang="scss">
+  .timeline {
+    margin-top: 2em;
+  }
+</style>

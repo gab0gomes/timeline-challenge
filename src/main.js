5c67245fe4801ed7c25d78e8a069e735511046f3
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2020-01-30 23:04:26 -0300

    Adiciona vue-svgicon ao projeto

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
+import SvgIcon from 'vue-svgicon';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.use(SvgIcon, {
+  tagName: 'svgicon',
+});
 
 new Vue({
   router,

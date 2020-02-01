9f9b7d81a51e4ab9313f4a77d7646f1e7a06b596
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2020-01-31 22:39:51 -0300

    Cria configuração da table da timeline

---
+++ src/configs/timelineTable.js
@@ -0,0 +1,14 @@
+export default {
+  fields: [
+    {
+      name: 'Produto',
+      key: 'product',
+      class: 'text-md',
+    },
+    {
+      name: 'Preço',
+      key: 'price',
+      class: 'text-md',
+    },
+  ],
+};

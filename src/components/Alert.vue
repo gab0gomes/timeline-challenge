547d567a942a81a012a219a9a004695cb0df8c58
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2020-02-01 18:57:10 -0300

    Cria componente de loading e de alert

---
+++ src/components/Alert.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="alert"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    classes: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.alert {
+  position: relative;
+  padding: .75rem 1.25rem;
+  margin-bottom: 1rem;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+}
+.alert-danger {
+  color: #721c24;
+  background-color: #f8d7da;
+  border-color: #f5c6cb;
+}
+.alert-secondary {
+  color: #383d41;
+  background-color: #e2e3e5;
+  border-color: #d6d8db;
+}
+</style>

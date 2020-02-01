4fcedc66a1fad3d55b63904c37703629eacfe458
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2020-02-01 15:37:29 -0300

    Ajusta responsividade

--- src/components/timeline/TimelineItem.vue
+++ src/components/timeline/TimelineItem.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="timeline-item"
-    :class="icon"
-  >
-    <div class="content">
-      <div class="header">
-        <slot name="header"/>
+  <div class="timeline-item-container">
+      <div class="timeline-item"
+        :class="icon"
+      >
+        <div class="content">
+          <div class="header">
+            <slot name="header"/>
+          </div>
+          <div class="body">
+            <slot />
+          </div>
+        </div>
       </div>
-      <div class="body">
-        <slot />
-      </div>
-    </div>
   </div>
 </template>
 
@@ -36,6 +38,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.timeline-item-container {
+  position: relative;
+  max-width: 1000px;
+  margin: 0 auto;
+
+  &::after { //ruler
+    content: '';
+    position: absolute;
+    width: 2px;
+    background-color: #D8D8D8;
+    top: 0;
+    bottom: 0;
+    left: 33%;
+    margin-left: -3px;
+  }
+}
+
 .timeline-item {
   padding: 10px 30px;
   position: relative;
@@ -47,14 +66,14 @@ export default {
     &::after {
       background-image: url('../../assets/fonts/icons/check.svg');
       background-repeat: no-repeat;
-      background-size: 1.5em;
+      background-size: 2em;
     }
   }
   &::after { //circles
     content: "";
     position: absolute;
-    width: 1.5em;
-    height: 1.5em;
+    width: 2em;
+    height: 2em;
     right: -15px;
     top: 20px;
     border-radius: 50%;
@@ -66,7 +85,7 @@ export default {
     content: " ";
     height: 0;
     position: absolute;
-    top: 22px;
+    top: 24px;
     width: 0;
     z-index: 1;
     left: 20px;
@@ -85,8 +104,6 @@ export default {
 
     .header {
       padding: 10px 20px 10px 20px ;
-
-      // height: 3em;
     }
 
     .body {
@@ -106,14 +123,61 @@ export default {
 
     &::before { //arrow
       left: 50px;
-      border: medium solid white;
-      border-width: 10px 10px 10px 0;
-      border-color: transparent white transparent transparent;
     }
 
     &::after {
       left: 15px;
     }
+  }
+
+  .timeline-item-container::after {
+    left: 31px;
+  }
+
+}
+@media screen and (min-width: 600px) and (max-width: 768px) {
+  .timeline-item  {
+    width: 80%;
+    padding-left: 60px;
+    padding-right: 25px;
+    left: 10%;
+
+    &::before { //arrow
+      left: 50px;
+    }
+
+    &::after { //circles
+      left: 15px;
+    }
+  }
+
+  .timeline-item-container::after { //ruler
+    left: 10%;
+    margin-left: 28px;
+
+  }
+}
+
+@media screen and (min-width: 768px) and (max-width: 992px) {
+  .timeline-item  {
+    width: 70%;
+    padding-left: 60px;
+    padding-right: 25px;
+    left: 15%;
+
+    &::before { //arrow
+      left: 50px;
+    }
+
+    &::after { //circles
+      left: 15px;
+    }
+  }
+
+  .timeline-item-container::after { //ruler
+    left: 15%;
+    margin-left: 28px;
+
   }
 }
 </style>

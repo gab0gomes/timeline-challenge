6ea159c34f67fcd0a5059d583776094260bfd3dc
Gabriel Rafael <gabrielrafael2508@hotmail.com>
2020-01-31 22:39:29 -0300

    Organiza css e cria variaveis

--- src/App.vue
+++ src/App.vue
@@ -17,19 +17,4 @@ export default {
 
 <style lang="scss">
   @import './assets/styles/app.scss';
-
-  * {
-    box-sizing: border-box;
-  }
-
-  body {
-    background-color: #EDF0FC;
-    font-family: 'Roboto', sans-serif;
-    font-size: 16px;
-    color: #555;
-  }
-
-  #app {
-
-  }
 </style>
